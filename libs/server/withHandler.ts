f4deedd1d55283633c096ca054ae29677a473dae
JJun <futuregoing46@gmail.com>
2022-07-05 20:06:39 +0900

    swr 적용

--- libs/server/withHandler.ts
+++ libs/server/withHandler.ts
@@ -1,16 +1,28 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default function withHandler(
-  method: 'GET' | 'POST' | 'DELETE',
-  fn: (req: NextApiRequest, res: NextApiResponse) => void,
-) {
-  return async function (req: NextApiRequest, res: NextApiResponse) {
+interface IConfig {
+  method: 'GET' | 'POST' | 'DELETE';
+  handler: (req: NextApiRequest, res: NextApiResponse) => void;
+  isPrivate?: boolean;
+}
+
+export default function withHandler(config: IConfig) {
+  return async function (
+    req: NextApiRequest,
+    res: NextApiResponse,
+  ): Promise<any> {
+    const { method, handler, isPrivate = true } = config;
+
     if (req.method !== method) {
       return res.status(405).end();
     }
 
+    if (isPrivate && !req.session.user) {
+      return res.status(401).json({ ok: false, message: 'please login' });
+    }
+
     try {
-      await fn(req, res);
+      await handler(req, res);
     } catch (e) {
       return res.status(500).json({ error: e });
     }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,22 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { SWRConfig } from 'swr';
+
+const fetcher = async (uri: string) => {
+  const response = await fetch(uri);
+  const json = await response.json();
+
+  return json;
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <SWRConfig value={{ fetcher }}>
+      <div className="w-full max-w-xl mx-auto">
+        <Component {...pageProps} />
+      </div>
+    </SWRConfig>
+  );
 }
 
-export default MyApp
+export default MyApp;
