744d78496cfd741e8a73bb603c338f601633fda2
JJun <futuregoing46@gmail.com>
2022-07-07 20:57:03 +0900

    리뷰

---
+++ pages/api/reviews.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler from 'libs/server/withHandler';
+import client from 'libs/server/client';
+import { withSession } from 'libs/server/withSession';
+
+interface IResponseType {
+  ok: boolean;
+  [key: string]: any;
+}
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<IResponseType>,
+) {
+  const {
+    session: { user },
+  } = req;
+
+  const reviews = await client.review.findMany({
+    where: {
+      createdForId: user?.id,
+    },
+    include: {
+      createdBy: true,
+    },
+  });
+
+  res.json({
+    ok: true,
+    reviews,
+  });
+}
+
+export default withSession(
+  withHandler({
+    methods: ['GET'],
+    handler,
+  }),
+);
