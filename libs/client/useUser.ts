b34e25b55d961438bb1fe7ca093d47326479a006
JJun <futuregoing46@gmail.com>
2022-07-07 20:57:44 +0900

    유저 정보 훅

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,13 +1,18 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import { User } from '@prisma/client';
+
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
 
 export default function useUser() {
   const [user, setUser] = useState();
   const router = useRouter();
-  console.log('router 처음에 un인가?', router);
 
-  const { data, error } = useSWR('/api/users/me');
+  const { data, error } = useSWR<ProfileResponse>('/api/users/me');
 
   useEffect(() => {
     if (data && !data.ok) {
