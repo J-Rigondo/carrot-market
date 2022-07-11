1e0fc27ef271171b67693152727fe7e5f67b2689
JJun <futuregoing46@gmail.com>
2022-07-11 16:16:12 +0900

    라이브 스트림

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -19,10 +19,7 @@ async function handler(
   } = req;
 
   if (req.method === 'GET') {
-    const streams = await client.stream.findMany({
-      skip: 10,
-      take: 10,
-    });
+    const streams = await client.stream.findMany();
 
     res.json({
       ok: true,
@@ -30,11 +27,34 @@ async function handler(
     });
   }
   if (req.method === 'POST') {
+    const response = await fetch(
+      `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ID}/stream/live_inputs`,
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${process.env.CLOUDFLARE_STREAM_TOKEN}`,
+          body: '{"meta": {"name":"test stream 1"},"recording": { "mode": "automatic", "timeoutSeconds": 10 }}',
+        },
+      },
+    );
+    console.log(response);
+
+    const {
+      result: {
+        uid,
+        rtmps: { url, streamKey },
+      },
+    } = await response.json();
+    console.log(uid, url, streamKey);
+
     let stream;
 
     try {
       stream = await client.stream.create({
         data: {
+          cloudflareKey: streamKey,
+          cloudflareId: uid,
+          cloudflareUrl: url,
           name,
           price: +price,
           description,
