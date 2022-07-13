33683cf9b8eca989ce8b43ca51e60b4df306a36f
JJun <futuregoing46@gmail.com>
2022-07-13 14:37:30 +0900

    Next script를 로드하는 법

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SWRConfig } from 'swr';
+import Script from 'next/script';
 
 const fetcher = async (uri: string) => {
   const response = await fetch(uri);
@@ -15,6 +16,23 @@ function MyApp({ Component, pageProps }: AppProps) {
       <div className="w-full max-w-xl mx-auto">
         <Component {...pageProps} />
       </div>
+      <Script
+        src="https://developers.kakao.com/sdk/js/kakao.js"
+        strategy="lazyOnload"
+      />
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        onLoad={() => {
+          window.fbAsyncInit = function () {
+            FB.init({
+              appId: 'your-app-id',
+              autoLogAppEvents: true,
+              xfbml: true,
+              version: 'v14.0',
+            });
+          };
+        }}
+      />
     </SWRConfig>
   );
 }
